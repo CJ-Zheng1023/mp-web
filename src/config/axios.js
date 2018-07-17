cd398d576b0b5a7a7319f9dc3bbc4d69a8bf807d
AfterWin <CJ_Zheng1023@hotmail.com>
2018-07-17 11:55:02 +0800

    修改各种BUG

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,27 +1,34 @@
 import axios from 'axios'
 import GLOBAL from './global'
+import Vue from 'vue'
 import router from '../router/index'
 axios.defaults.timeout = 15000
 axios.defaults.baseURL = GLOBAL.BACK_END_URL
 axios.interceptors.response.use(
   response => {
-    let errorCode = response.data['error']
-    if (errorCode) {
-      switch (errorCode) {
-        case '1':
-          router.replace({path: '/'})
-          break
-        default:
-          router.replace({path: '/'})
-      }
-    }
     return response
   },
   error => {
-    window.localStorage.removeItem('token')
-    window.localStorage.removeItem('userId')
-    window.localStorage.removeItem('username')
-    router.replace({path: '/'})
+    if (error.response) {
+      switch (error.response.status) {
+        case 403:
+          window.localStorage.removeItem('token')
+          window.localStorage.removeItem('userId')
+          window.localStorage.removeItem('username')
+          Vue.prototype.$alert('您的登录超时，请重新登录', '提示', {
+            confirmButtonText: '确定',
+            type: 'error'
+          }).then(action => {
+            router.replace({path: '/'})
+          })
+          break
+        default:
+          Vue.prototype.$alert(`系统出了点问题。。。`, '提示', {
+            confirmButtonText: '确定',
+            type: 'error'
+          })
+      }
+    }
     return Promise.reject(error)
   }
 )
